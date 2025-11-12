e03a4b034ebc85511db72eb7dcc8fa6668deb26f
Furkan Kuyumcu <fkuyumcu@k1m04s09.42istanbul.com.tr>
2025-11-12 16:27:31 +0300

    env

--- srcs/requirements/mariadb/tools/init_mariadb.sh
+++ srcs/requirements/mariadb/tools/init_mariadb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Create the run directory for MariaDB socket
+
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
-# Start MariaDB in the background
+#bg
 mysqld --user=mysql --datadir=/var/lib/mysql &
 
-# Wait for MariaDB to be ready
+
 until mysqladmin ping --silent; do
     echo 'Waiting for MariaDB to be ready...'
     sleep 2
